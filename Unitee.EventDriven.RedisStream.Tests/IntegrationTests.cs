b67b50feb1d215394e44e9f0e9c7f77bb05eba2b
Titouan CREACH <t.creach@unitee.io>
2022-11-21 10:39:23 +0100

    set up a basic test

--- Unitee.EventDriven.RedisStream.Tests/IntegrationTests.cs
+++ Unitee.EventDriven.RedisStream.Tests/IntegrationTests.cs
@@ -1,19 +1,53 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Moq;
+using StackExchange.Redis;
+using Unitee.EventDriven.Abstraction;
+using Unitee.EventDriven.Attributes;
+using Unitee.RedisStream;
+
 namespace Unitee.EventDriven.RedisStream.Tests;
 
-public class UnitTest1 : IClassFixture<CustomWebApplicationFactory>
-{
-    private readonly CustomWebApplicationFactory _factory;
+[Subject("TEST_EVENT")]
+public record TestEvent(string ATestString);
 
-    public UnitTest1(CustomWebApplicationFactory factory)
+public class UnitTest1
+{
+    public UnitTest1()
     {
-        _factory = factory;
     }
 
     [Fact]
     public async Task Test1()
     {
-        var client = _factory.Server.CreateClient();
-        var resp = await client.SendAsync(new HttpRequestMessage(HttpMethod.Get, "/"));
-        Console.WriteLine(resp.StatusCode);
+        IServiceCollection services = new ServiceCollection();
+        var redis = await ConnectionMultiplexer.ConnectAsync("localhost:6379");
+
+        services.AddLogging();
+        services.AddSingleton<IConnectionMultiplexer>(redis);
+        services.AddScoped<IRedisStreamPublisher, RedisStreamPublisher>();
+
+        services.AddScoped(provider => new RedisStreamMessagesProcessor("Test", provider,
+            provider.GetRequiredService<IConnectionMultiplexer>(),
+            provider.GetRequiredService<ILogger<RedisStreamMessagesProcessor>>()));
+
+        var consumerInstance = Mock.Of<IRedisStreamConsumer<TestEvent>>();
+
+        services.AddSingleton<RedisStreamBackgroundReceiver>(x => new RedisStreamBackgroundReceiver(x, "Test"));
+        services.AddSingleton<IConsumer>(consumerInstance);
+
+        var provider = services.BuildServiceProvider();
+
+        var publisher = provider.GetRequiredService<IRedisStreamPublisher>();
+
+        await publisher.PublishAsync(new TestEvent("World"));
+
+        var backgroundService = provider.GetService<RedisStreamBackgroundReceiver>();
+        await backgroundService.StartAsync(CancellationToken.None);
+        await Task.Delay(1000);
+        await backgroundService.StopAsync(CancellationToken.None);
+
+        var moc = Mock.Get(consumerInstance);
+        moc.Verify(x=> x.ConsumeAsync(new TestEvent("World")), Times.Once);
     }
 }
